da1bf9e3e9bc3f60277b3d8e4b1c60da07344100
mango2017 <1210056515@qq.com>
2017-07-10 14:24:21 +0800

    hehehe

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
 <?php
-echo "hello git 20170710111";
+
+echo 1;
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 
